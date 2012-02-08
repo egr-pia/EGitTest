7a5153686cb7cd5c0495f3239af55bc725a40a10
Elsa Greder <egr@pia-conseil.com>
2012-02-08 17:22:31 +0100

    test egit

--- EGitTest/src/HelloWorld.java
+++ EGitTest/src/HelloWorld.java
@@ -6,7 +6,8 @@ public class HelloWorld
    */
   public static void main(String[] args)
   {
-    System.out.println("Hello world - test egit with computeClass dans la branche dev2" + Compute.compute(1, 2));
+	  System.out.println("Toto");
+    System.out.println("Hello world - test egit with computeClass static" + Compute.compute(1, 2));
   }
 
 }
