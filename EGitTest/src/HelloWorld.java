06c4a0564b11b29d88c252a0ce16c6201f03f8e4
Elsa Greder <egr@pia-conseil.com>
2012-02-08 14:38:14 +0100

    Test update

--- EGitTest/src/HelloWorld.java
+++ EGitTest/src/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld
    */
   public static void main(String[] args)
   {
-    System.out.println("Hello world - test egit");
+    System.out.println("Hello world - test egit 222");
 
   }
 
