e6def4f747dc57a533865301435994e1232b7df0
Elsa Greder <egr@pia-conseil.com>
2012-02-08 17:41:25 +0100

    Commit egit

--- EGitTest/src/HelloWorld.java
+++ EGitTest/src/HelloWorld.java
@@ -6,8 +6,9 @@ public class HelloWorld
    */
   public static void main(String[] args)
   {
-	  System.out.println("Toto");
+    System.out.println("Toto");
     System.out.println("Hello world - test egit with computeClass dans la branche dev2" + Compute.compute(1, 2));
+    System.out.println("Tata");
   }
 
 }
