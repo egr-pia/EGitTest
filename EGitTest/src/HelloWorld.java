caebebc3929d708f6835aa00d2263dca2f648cbe
Elsa Greder <egr@pia-conseil.com>
2012-02-08 15:15:57 +0100

    Compute class

--- EGitTest/src/HelloWorld.java
+++ EGitTest/src/HelloWorld.java
@@ -6,7 +6,8 @@ public class HelloWorld
    */
   public static void main(String[] args)
   {
-    System.out.println("Hello world - test egit 4");
+  	
+    System.out.println("Hello world - test egit with computeClass");
 
   }
 
