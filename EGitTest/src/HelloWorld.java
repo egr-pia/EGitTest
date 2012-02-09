7ead60ba26190a80f6645003dc7f0c0f5d588a14
Elsa Greder <egr@pia-conseil.com>
2012-02-09 10:40:52 +0100

    Test merge

--- EGitTest/src/HelloWorld.java
+++ EGitTest/src/HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld
   public static void main(String[] args)
   {
     System.out.println("Hello world - test egit with computeClass dans la branche dev2" + Compute.compute(1, 2));
-    System.out.println("Tata");
+    System.out.println("Toto");
   }
 
 }
