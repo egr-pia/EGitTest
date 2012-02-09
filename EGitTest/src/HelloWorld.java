365856395029b82fe9e43d9e18f6d678c1e88ba2
Elsa Greder <egr@pia-conseil.com>
2012-02-09 11:13:54 +0100

    Test merge command line side

--- EGitTest/src/HelloWorld.java
+++ EGitTest/src/HelloWorld.java
@@ -7,8 +7,6 @@ public class HelloWorld
   public static void main(String[] args)
   {
     System.out.println("Hello world" + Compute.compute(1, 5));
-    System.out.println("Toto");
-    System.out.println("End");
   }
 
 }
